05ff17691f4960b986d45e80f3afa3a52b8597a4
Christian Dresel <fff@chrisi01.de>
2018-09-05 17:10:51 +0200

    Add Captcha
    Signed-off-by: Christian Dresel <fff@chrisi01.de>

--- function.php
+++ function.php
@@ -11,4 +11,32 @@ function getrout($ip)
 		return false;
 	}
 }
-		
+
+function zahlinwort($zahl)
+{
+	if ($zahl==0) { return "null"; }
+        if ($zahl==1) { return "eins"; }
+        if ($zahl==2) { return "zwei"; }
+        if ($zahl==3) { return "drei"; }
+        if ($zahl==4) { return "vier"; }
+        if ($zahl==5) { return "fünf"; }
+        if ($zahl==6) { return "sechs"; }
+        if ($zahl==7) { return "sieben"; }
+        if ($zahl==8) { return "acht"; }
+        if ($zahl==9) { return "neun"; }
+}
+function captcha()
+{
+	$zahl1=rand(0,9);
+	$zahl2=rand(0,9);
+	$zeichen=rand(1,2);
+	if ($zeichen==1) { 
+		$zeichen1="+"; 
+		$_SESSION["erg"]=$zahl1+$zahl2;
+	}
+        if ($zeichen==2) { 
+		$zeichen1="*"; 
+                $_SESSION["erg"]=$zahl1*$zahl2;
+	}
+	return ''.$zahl1.' '.$zeichen1.' '.$zahl2.' ergibt:';
+}
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include("config.php");
-if ($_GET['assign'] == true)
+include("function.php");
+if ($_SESSION["erg"] == $_POST['erg'])
 {
 	$ergebnis = mysqli_query($db, "SELECT ip FROM ip WHERE assign = '0' LIMIT 0,1");
 	while($row = mysqli_fetch_object($ergebnis))
@@ -13,5 +15,15 @@ if ($_GET['assign'] == true)
 	$aendern = "UPDATE ip Set assign = '1' WHERE ip = '$ip'";
 	$update = mysqli_query($db, $aendern);
 }
-?>
-Klicke <a href="index.php?assign=true">hier</a> um eine IP Adresse zu assignen
+else
+{
+	?>
+	Bitte die Rechnung lösen um eine IP zu assignen:<br />
+	<?php
+	echo captcha();
+	?>
+	<form action="index.php" method="post">
+	<input type="text" size="17" name="erg">
+	<input type="submit" value="assign">
+	</form>
+<?php } ?>
