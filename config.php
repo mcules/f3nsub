e2d9fd54a8f87aa147127f00a421f48fe372b371
Christian Dresel <fff@chrisi01.de>
2018-09-05 17:56:50 +0200

    Add api
    Signed-off-by: Christian Dresel <fff@chrisi01.de>

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 
 $mysqluser="f3nsub";
@@ -14,5 +14,6 @@ if(!$db)
 }
 
 $initaktiv=0;
+$abgelaufen_zeit=365*24*60*60; //In Sekunden
 
 ?>
