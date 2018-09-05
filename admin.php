2051480ad43ae187f0762d7cb7f3d9a95249f7aa
Christian Dresel <fff@chrisi01.de>
2018-09-05 17:20:10 +0200

    Move act_announce to cron and database
    Signed-off-by: Christian Dresel <fff@chrisi01.de>

--- admin.php
+++ admin.php
@@ -4,13 +4,14 @@ ini_set('display_errors', 1);
 include("config.php");
 include("function.php");
 $ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1'");
+echo "Die aktuellen Announcements sowie last_announce wird nur 1x täglich aktualisiert";
 echo "<table><tr><td>ip</td><td>last_announce</td>";
 while($row = mysqli_fetch_object($ergebnis))
 {
 	$datum = date("d.m.Y",$row->last_announce);
 	$uhrzeit = date("H:i",$row->last_announce);
 	$zeit = ''.$datum.' - '.$uhrzeit.' Uhr';
-	if(getrout($row->ip))
+	if($row->act_announce == 1)
 	{
 		echo '<tr><td>'.$row->ip.'</td><td>'.$zeit.'</td><td>Wird aktuell announced</td></tr>';
 	}
--- cron.php
+++ cron.php
@@ -4,12 +4,14 @@ ini_set('display_errors', 1);
 include("config.php");
 include("function.php");
 $time=time();
+$aendern = "UPDATE ip Set act_announce = '0'";
+$update = mysqli_query($db, $aendern);
 $ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1'");
 while($row = mysqli_fetch_object($ergebnis))
 {
 	if(getrout($row->ip))
 	{
-		$aendern = "UPDATE ip Set last_announce = '$time' WHERE id = '$row->id'";
+		$aendern = "UPDATE ip Set last_announce = '$time', act_announce = '1' WHERE id = '$row->id'";
 		$update = mysqli_query($db, $aendern);
 	}
 }
