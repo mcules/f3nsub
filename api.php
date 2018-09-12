61ef3eba28a06e56cde793027e28bfc911d41109
Christian Dresel <fff@chrisi01.de>
2018-09-12 11:53:45 +0200

    API gekürzt
    Signed-off-by: Christian Dresel <fff@chrisi01.de>

--- api.php
+++ api.php
@@ -4,67 +4,34 @@
 include("config.php");
 include("function.php");
 $data = array();
+$abgelaufen=time()-$abgelaufen_zeit;
 if ($_GET['abfrage'] == "all")
 {
 	$ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1'");
-	while($row = mysqli_fetch_object($ergebnis))
-	{
-		$datas = array();
-		$datas['ip'] = $row->ip;
-		$datas['act_announce'] = $row->act_announce;
-		$datas['last_announce'] = $row->last_announce;
-		array_push($data, $datas);
-	}
 }
 if ($_GET['abfrage'] == "aktiv")
 {
         $ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1' AND act_announce = '1'");
-        while($row = mysqli_fetch_object($ergebnis))
-        {
-                $datas = array();
-                $datas['ip'] = $row->ip;
-                $datas['act_announce'] = $row->act_announce;
-                $datas['last_announce'] = $row->last_announce;
-                array_push($data, $datas);
-        }
 }
 if ($_GET['abfrage'] == "inaktiv")
 {
         $ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1' AND act_announce = '0'");
-        while($row = mysqli_fetch_object($ergebnis))
-        {
-                $datas = array();
-                $datas['ip'] = $row->ip;
-                $datas['act_announce'] = $row->act_announce;
-                $datas['last_announce'] = $row->last_announce;
-                array_push($data, $datas);
-        }
 }
 if ($_GET['abfrage'] == "abgelaufen")
 {
-	$abgelaufen=time()-$abgelaufen_zeit;
         $ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1' AND act_announce = '0' AND last_announce < '$abgelaufen' AND last_announce > '1'");
-        while($row = mysqli_fetch_object($ergebnis))
-        {
-                $datas = array();
-                $datas['ip'] = $row->ip;
-                $datas['act_announce'] = $row->act_announce;
-                $datas['last_announce'] = $row->last_announce;
-                array_push($data, $datas);
-        }
 }
 if ($_GET['abfrage'] == "nieaktiv")
 {
-        $abgelaufen=time()-$abgelaufen_zeit;
         $ergebnis = mysqli_query($db, "SELECT * FROM ip WHERE assign = '1' AND act_announce = '0' AND last_announce = '0'");
-        while($row = mysqli_fetch_object($ergebnis))
-        {
-                $datas = array();
-                $datas['ip'] = $row->ip; 
-                $datas['act_announce'] = $row->act_announce;
-                $datas['last_announce'] = $row->last_announce;
-                array_push($data, $datas);
-        }
+}
+while($row = mysqli_fetch_object($ergebnis))
+{
+	$datas = array();
+	$datas['ip'] = $row->ip;
+	$datas['act_announce'] = $row->act_announce;
+	$datas['last_announce'] = $row->last_announce;
+	array_push($data, $datas);
 }
 header("Content-Type: application/json");
 echo json_encode($data, JSON_PRETTY_PRINT);
